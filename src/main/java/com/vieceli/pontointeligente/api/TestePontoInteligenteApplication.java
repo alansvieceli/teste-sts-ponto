502a10530b7359c66b87ece740321873370dd4d9
Alan Vieceli <alansvieceli@gmail.com>
2018-10-20 19:36:35 -0300

    Inicio do Projeto

---
+++ src/main/java/com/vieceli/pontointeligente/api/TestePontoInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.vieceli.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePontoInteligenteApplication.class, args);
+	}
+}
